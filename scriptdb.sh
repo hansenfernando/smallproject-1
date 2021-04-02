a574279bdb3b9bcb84d9b340f8dbbc4130fe62cb
vagrant <vagrant@db>
2021-04-02 17:53:32 +0000

    Adding some configure line to automate installation MySQL

--- scriptdb.sh
+++ scriptdb.sh
@@ -9,5 +9,14 @@ echo "Installing MySQL Server           "
 echo "=================================="
 sudo apt-get install mysql-server -y
 echo "=================================="
-echo "MySQL is ready to serve..........."
+echo "Installation for mysql finished..."
 echo "=================================="
+echo "Setting bind-address MySQL........"
+echo "=================================="
+sudo wget https://raw.githubusercontent.com/hansenfernando/smallproject-1/main/mysqld.cnf -O /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "=================================="
+echo "Restarting MySQL service.........."
+echo "=================================="
+sudo systemctl restart mysql
+echo "=================================="
+echo "MySQL is ready to serve..........."
