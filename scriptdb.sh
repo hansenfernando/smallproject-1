19ea9552b325165e6966c990a917786844404a97
vagrant <vagrant@db>
2021-04-02 17:02:29 +0000

    creating automation install MySQL server scrip

---
+++ scriptdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "=================================="
+echo "Preparing MySQL Server Instalation"
+echo "=================================="
+sudo apt-get update
+echo "=================================="
+echo "Installing MySQL Server           "
+echo "=================================="
+sudo apt-get install mysql-server
+echo "=================================="
+echo "MySQL is ready to serve..........."
+echo "=================================="
